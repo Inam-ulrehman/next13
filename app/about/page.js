2ff2e0ecd0c816d0fcc05139fc3577b3086adb0c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-15 07:47:26 -0400

    fix

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+export const metadata = {
+  title: 'About us',
+  description: 'This is about us page',
+}
 const About = () => {
   return <div>About</div>
 }
---
+++ app/samples/[id]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SingleSample = ({ params }) => {
+  return <div>SingleSample: {params.id} </div>
+}
+
+export default SingleSample
