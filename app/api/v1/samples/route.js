0866336544425045d375f19de3e09d075cc96651
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-19 14:47:47 -0400

    fix

--- app/api/v1/samples/route.js
+++ app/api/v1/samples/route.js
@@ -5,7 +5,6 @@ export async function GET(request) {
   const pathName = request.nextUrl.pathname
   const searchParams = request.nextUrl.searchParams
   const cookies = request.cookies.getAll()
-  const body = await request.json()
 
   await dbConnect()
   try {
