73ef345b658f0c69382b7a8d571e311a5769716c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-20 09:17:32 -0400

    fix

--- app/providers.js
+++ app/providers.js
@@ -1,10 +1,11 @@
 'use client'
 
 import { CacheProvider } from '@chakra-ui/next-js'
-
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/toast'
 import { theme } from './styles/theme'
+import { Provider } from 'react-redux'
+import store from '@/store'
 
 const { ToastContainer } = createStandaloneToast()
 
@@ -12,10 +13,12 @@ export function Providers({ children }) {
   return (
     <>
       <CacheProvider>
-        <ChakraProvider theme={theme}>
-          {children}
-          <ToastContainer />
-        </ChakraProvider>
+        <Provider store={store}>
+          <ChakraProvider theme={theme}>
+            {children}
+            <ToastContainer />
+          </ChakraProvider>
+        </Provider>
       </CacheProvider>
     </>
   )
