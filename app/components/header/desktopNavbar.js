d8cf6c58469df545b93b2a06da876a06863d8cb0
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-20 12:09:57 -0400

    fix

--- app/components/header/desktopNavbar.js
+++ app/components/header/desktopNavbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from '@chakra-ui/next-js'
-import { List, ListItem } from '@chakra-ui/react'
+import { Box, Button, LinkBox, List, ListItem } from '@chakra-ui/react'
 import styled from '@emotion/styled'
+import DesktopLogo from './desktopLogo'
+import { Row } from '@nextui-org/react'
 const DesktopNavbar = () => {
   return (
     <Wrapper>
+      <DesktopLogo />
       <List display={'flex'} gap={5}>
         <ListItem>
           <Link href={'/'}>Home</Link>
@@ -16,10 +19,22 @@ const DesktopNavbar = () => {
           <Link href={'/samples'}>Samples</Link>
         </ListItem>
       </List>
+      <Box>
+        <Link mr={2} href={'login'}>
+          <Button>Login</Button>
+        </Link>
+        <Link href={'/register'}>
+          <Button>Register</Button>
+        </Link>
+      </Box>
     </Wrapper>
   )
 }
 const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  background-color: var(--chakra-colors-white);
   @media (max-width: 768px) {
     display: none;
   }
