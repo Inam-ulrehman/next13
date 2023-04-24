1d1aa73620fa30148d1b8d75a763a1f925e16318
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-24 15:04:09 -0400

    fix

--- app/components/header/isMember.js
+++ app/components/header/isMember.js
@@ -1,27 +1,44 @@
 import { Link } from '@chakra-ui/next-js'
 import { Box, Button, ButtonGroup } from '@chakra-ui/react'
-import React from 'react'
+import styled from '@emotion/styled'
+import Cookies from 'js-cookie'
+import React, { useEffect, useState } from 'react'
 
+const initialState = {
+  show: true,
+}
 const IsMember = () => {
-  return (
-    <div>
-      <Box>
-        <ButtonGroup isAttached>
-          <Button
-            as={Link}
-            href={'/login'}
-            borderRight='2px solid var(--chakra-colors-gray-300)'
-          >
-            Login
-          </Button>
+  const [state, setState] = useState(initialState)
+  const token = Cookies.get('Authorization_Token')
 
-          <Button as={Link} href={'/register'}>
-            Register
-          </Button>
-        </ButtonGroup>
+  useEffect(() => {
+    if (!token || token === 'undefined') {
+      return setState({ ...state, show: true })
+    }
+    setState({ ...state, show: false })
+  }, [token])
+  return (
+    <Wrapper>
+      <Box>
+        {state.show && (
+          <ButtonGroup isAttached>
+            <Button
+              as={Link}
+              href={'/login'}
+              borderRight='2px solid var(--chakra-colors-gray-300)'
+            >
+              Login
+            </Button>
+
+            <Button as={Link} href={'/register'}>
+              Register
+            </Button>
+          </ButtonGroup>
+        )}
       </Box>
-    </div>
+    </Wrapper>
   )
 }
+const Wrapper = styled.div``
 
 export default IsMember
