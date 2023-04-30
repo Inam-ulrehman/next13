17b23e50d0a39a2cdfffcf63e4098aa70de0dd2c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-30 11:16:49 -0400

    fix

--- app/cars/filterYear.js
+++ app/cars/filterYear.js
@@ -6,49 +6,107 @@ import {
   AccordionPanel,
   Box,
   Input,
+  InputGroup,
+  InputLeftAddon,
   RangeSlider,
   RangeSliderFilledTrack,
   RangeSliderThumb,
   RangeSliderTrack,
 } from '@chakra-ui/react'
 import styled from '@emotion/styled'
-import React from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import React, { useState } from 'react'
 
-const Year = () => {
+const initialState = {
+  yearStart: 2000,
+  yearEnd: 2023,
+}
+const Price = () => {
+  const searchParams = useSearchParams()
+  const path = usePathname()
+  const router = useRouter()
+  const [state, setState] = useState(initialState)
+  const { yearStart, yearEnd } = state
+  function mapRange(value, oldMin, oldMax, newMin, newMax) {
+    const oldRange = oldMax - oldMin
+    const newRange = newMax - newMin
+    const scaledValue = (value - oldMin) / oldRange
+    const newValue = newMin + scaledValue * newRange
+    return Math.round(newValue)
+  }
+  const handleValue = (value) => {
+    const yearStart = mapRange(value[0], 1, 100, 2000, 2023)
+    const yearEnd = mapRange(value[1], 1, 100, 2000, 2023)
+
+    setState({ ...state, yearStart, yearEnd })
+  }
+
+  const handleValueEnd = (value) => {
+    const yearStart = mapRange(value[0], 1, 100, 2000, 2023)
+    const yearEnd = mapRange(value[1], 1, 100, 2000, 2023)
+    const otherQuery = searchParams
+      .toString()
+      .split('&')
+      .filter((item) => {
+        return !item.startsWith('yearStart') && !item.startsWith('yearEnd')
+      })
+      .join('&')
+      .replace(/%2C/g, ',')
+    if (otherQuery) {
+      return router.replace(
+        `${path}?yearStart=${yearStart}&yearEnd=${yearEnd}&${otherQuery}`
+      )
+    }
+
+    router.replace(`${path}?yearStart=${yearStart}&yearEnd=${yearEnd}`)
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setState({ ...state, [name]: value })
+  }
   return (
     <Wrapper>
       <Accordion allowToggle>
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as='p' flex='1' textAlign='left' fontWeight={'medium'}>
+              <Box as='span' flex='1' textAlign='left' fontWeight={'medium'}>
                 Year
-                <Box
-                  as='span'
-                  display={'block'}
-                  flex='1'
-                  textAlign='left'
-                  fontWeight={'light'}
-                >
-                  2008 - 2023
-                </Box>
               </Box>
-
               <AccordionIcon />
             </AccordionButton>
           </h2>
           <AccordionPanel>
-            <div className='year-range-input'>
+            <div className='price-range-input'>
               <p>price range</p>
               <div className='amount'>
-                <Input type='text' />
-                <Input type='text' />
+                <Input
+                  isReadOnly={true}
+                  type='text'
+                  value={yearStart}
+                  name='yearStart'
+                  id='yearStart'
+                  onChange={handleChange}
+                />
+
+                <Input
+                  isReadOnly={true}
+                  type='text'
+                  value={yearEnd}
+                  name='yearEnd'
+                  id='yearEnd'
+                  onChange={handleChange}
+                />
               </div>
             </div>
             <RangeSlider
               aria-label={['min', 'max']}
-              colorScheme='pink'
-              defaultValue={[10, 30]}
+              colorScheme='red'
+              defaultValue={[20, 70]}
+              onChangeEnd={(val) => handleValueEnd(val)}
+              onChange={(val) => handleValue(val)}
             >
               <RangeSliderTrack>
                 <RangeSliderFilledTrack></RangeSliderFilledTrack>
@@ -57,11 +115,11 @@ const Year = () => {
               <RangeSliderThumb index={1}></RangeSliderThumb>
             </RangeSlider>
             <div
-              className='year-range'
+              className='price-range'
               style={{ display: 'flex', justifyContent: 'space-between' }}
             >
-              <span>$0</span>
-              <span>$10000.00</span>
+              <span>Start - {yearStart}</span>
+              <span>End - {yearEnd}</span>
             </div>
           </AccordionPanel>
         </AccordionItem>
@@ -70,5 +128,20 @@ const Year = () => {
   )
 }
 
-const Wrapper = styled.div``
-export default Year
+const Wrapper = styled.div`
+  .price-range-input {
+    input {
+      max-width: 100px;
+    }
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .amount {
+      display: flex;
+      justify-content: space-between;
+
+      gap: 1rem;
+    }
+  }
+`
+export default Price
