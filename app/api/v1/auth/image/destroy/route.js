2e7cf159f2061b1a6189cff07f2e2480500aee2d
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-02 11:15:23 -0400

    fix

--- app/api/v1/auth/image/destroy/route.js
+++ app/api/v1/auth/image/destroy/route.js
@@ -3,14 +3,44 @@ import mongooseErrorHandler from '@/lib/errors/mongoose-error-handler'
 import { titleCase } from '@/lib/helper'
 import User from '@/models/User'
 import { StatusCodes } from 'http-status-codes'
-
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+})
 export async function POST(request, res) {
   const pathName = request.nextUrl.pathname
   const searchParams = request.nextUrl.searchParams
   const cookies = request.cookies.getAll()
-  const body = await request.json()
-
-  console.log(body)
   await dbConnect()
-  return new Response('hello file delete')
+  const body = await request.json()
+  console.log(body)
+  const { public_id } = body
+
+  try {
+    const result = await cloudinary.uploader.destroy(public_id)
+    return new Response(
+      JSON.stringify({
+        success: false,
+        msg: `Successfully deleted`,
+        result,
+      }),
+      {
+        status: StatusCodes.OK,
+      }
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        msg: `Unable to delete.`,
+        result: error.result,
+      }),
+      {
+        status: StatusCodes.BAD_REQUEST,
+      }
+    )
+  }
 }
