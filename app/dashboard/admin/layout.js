3dadce5c9fb26a832dbce38d9e9978799968d9ff
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-02 07:26:15 -0400

    fix

--- app/dashboard/admin/layout.js
+++ app/dashboard/admin/layout.js
@@ -1,8 +1,10 @@
 import DashboardNav from './dashboardNav'
-import Section from './section'
 
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}) {
-  return <Section children={children} />
+export default function DashboardLayout({ children }) {
+  return (
+    <>
+      <DashboardNav />
+      {children}
+    </>
+  )
 }
